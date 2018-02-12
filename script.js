108bfb61a91d89baa1534a9c99ebea55685ec17e
jenath <jenathkanagesan@gmail.com>
2018-02-12 22:50:22 +0000

    modified css

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ function backspace(){
   var minusOne=field.value.substring(0,field.value.length-1);
   document.getElementById('field').value=minusOne;
 }
+
+var right=document.getElementById('right_side');
